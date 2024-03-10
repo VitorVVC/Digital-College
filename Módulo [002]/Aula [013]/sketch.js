1c54377eb1b2dcd33fd8e555a776f6b165c37e25
Vitor <vitorvvc2005@gmail.com>
2024-03-09 22:58:48 -0300

    Bug fix: Corrigindo desafio P5, para manter responsividade

--- Módulo [002]/Aula [013]/sketch.js
+++ Módulo [002]/Aula [013]/sketch.js
@@ -1,35 +1,38 @@
 let cores = [];
 let quadrados = [];
 
+// Forneca a altura e largura padrão 
+let alturaLarguraPadrao = 50;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(alturaLarguraPadrao * 6, alturaLarguraPadrao * 6);
   frameRate(1);
   cores = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255), color(255, 255, 0)];
 }
 
 function draw() {
+  noLoop();
   background(220);
-  
-  const alturaLarguraPadrao = 50;
-  
+
+
   for (let i = 0; i < 4; i++) {
     let posicaoValida = false;
     let randomX, randomY;
-    
+
     while (!posicaoValida) {
       randomX = random(width - alturaLarguraPadrao);
       randomY = random(height - alturaLarguraPadrao);
-      
+
       posicaoValida = true;
       for (let j = 0; j < quadrados.length; j++) {
         const outroQuadrado = quadrados[j];
-        if (dist(randomX, randomY, outroQuadrado.posX, outroQuadrado.posY) < alturaLarguraPadrao) {
+        if (dist(randomX, randomY, outroQuadrado.posX, outroQuadrado.posY) < alturaLarguraPadrao + alturaLarguraPadrao / 2) {
           posicaoValida = false;
           break;
         }
       }
     }
-    
+
     quadrados.push({ posX: randomX, posY: randomY });
     fill(cores[i]);
     rect(randomX, randomY, alturaLarguraPadrao, alturaLarguraPadrao);
